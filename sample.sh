3a5001ee9e0c7bdbba030e4eaa6414968752dd6d
root <root@ip-172-31-18-109.ap-southeast-1.compute.internal>
2019-09-24 06:45:56 +0000

    javamavenadded

--- sample.sh
+++ sample.sh
@@ -2,12 +2,12 @@
 apt-get update
 apt-get install git -y
 git --version
-apt-get insatll maven -y
-mvn --version
-apt-get install openjdk-8-jdk -y
-java -version
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.lits
+apt-get insatll openjdk-8-jdk -y
+java  -version
+apt-get install maven -y
+mvn  --version
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 apt-get install jenkins
 systemctl start jenkins
