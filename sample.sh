2c52fb38961d91a08bda082e721218173a976c20
govi321 <51965283+govi321@users.noreply.github.com>
2020-03-09 11:17:09 +0530

    added ainstall

--- sample.sh
+++ sample.sh
@@ -2,13 +2,13 @@
 apt-get update
 apt-get install git -y
 git --version
-apt-get insatll openjdk-8-jdk -y
+apt-get install openjdk-8-jdk -y
 java  -version
 apt-get install maven -y
 mvn  --version
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
-apt-get install jenkins
+apt-get install jenkins -y
 systemctl start jenkins
 systemctl status jenkins
